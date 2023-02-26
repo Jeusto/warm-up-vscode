29e8b2e764d6a26e99e7c2cfeb38846a4e3ecb85
Arhun Saday <arhunsad@gmail.com>
2023-02-26 19:58:43 +0100

    Extract functions from the main "activate" one

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // This script will be run within VS Code
 // It can access the main VS Code APIs directly
+
 import {
   Uri,
   WebviewPanel,
@@ -15,31 +16,35 @@ import registerCommands from "./modules/commands";
 // Init status bar icon
 let startButton: StatusBarItem;
 
-// Function called after activation event
+/**
+ * Function called after activation event
+ * @param context Extension context
+ */
 export function activate(context: ExtensionContext) {
   // Fetch data from json file
-  const fs = require("fs");
-  const rawdata = fs.readFileSync(
-    `${context.extensionPath}/webview/data.json`,
-    "utf8"
-  );
-  const data = JSON.parse(rawdata);
-  const words = data.words;
-  const codes = data.codes;
+  let { words, codes } = fetchData(context);
 
   // Add status bar icon
-  startButton = window.createStatusBarItem(StatusBarAlignment.Left, 1);
-  startButton.command = "warmUp.start";
-  startButton.tooltip = "Start typing test";
-  startButton.text = `$(record-keys) Warm Up`;
-
-  context.subscriptions.push(startButton);
-  startButton.show();
+  registerStatusBarIcon(context);
 
   // Register all the commands
   registerCommands(WarmupWebview, context, words, codes);
 
   // Register webview panel serializer
+  registerWebviewPanelSerializer(context, words, codes);
+}
+
+/**
+ *
+ * @param context
+ * @param words
+ * @param codes
+ */
+function registerWebviewPanelSerializer(
+  context: ExtensionContext,
+  words: Record<string, string[]>,
+  codes: Record<string, string[]>
+) {
   if (window.registerWebviewPanelSerializer) {
     // Make sure we register a serializer in activation event
     window.registerWebviewPanelSerializer(WarmupWebview.viewType, {
@@ -58,4 +63,36 @@ export function activate(context: ExtensionContext) {
       },
     });
   }
+}
+
+/**
+ *
+ */
+function registerStatusBarIcon(context: ExtensionContext) {
+  startButton = window.createStatusBarItem(StatusBarAlignment.Left, 1);
+  startButton.command = "warmUp.start";
+  startButton.tooltip = "Start typing test";
+  startButton.text = `$(record-keys) Warm Up`;
+
+  context.subscriptions.push(startButton);
+  startButton.show();
+}
+
+/**
+ *
+ * @param context
+ * @returns
+ */
+function fetchData(context: ExtensionContext) {
+  const fs = require("fs");
+  const rawdata = fs.readFileSync(
+    `${context.extensionPath}/webview/data.json`,
+    "utf8"
+  );
+
+  const data = JSON.parse(rawdata);
+  const words = data.words;
+  const codes = data.codes;
+
+  return { words, codes };
 }
